6b8fd6d3a28cd5f2b46085539b46de4d3d31717a
omkgiri <giriom26@gmail.com>
2025-03-16 14:28:00 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,20 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCF3HWnkOfafmvHSvkC8JBdbKa2k_YTvNQ",
+  authDomain: "netflic-clone-e08e7.firebaseapp.com",
+  projectId: "netflic-clone-e08e7",
+  storageBucket: "netflic-clone-e08e7.firebasestorage.app",
+  messagingSenderId: "179564905410",
+  appId: "1:179564905410:web:2ef2d24b336e8328ac4c5b"
+  };
+
+ 
+
+
+
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjY2I0NmRhOTc5OTdjMWZlZmI4MDNjNTlkZDI0MWVkZCIsIm5iZiI6MTc0MTI1MDQwNy43MjgsInN1YiI6IjY3Yzk1ZjY3NGFmOGE2ODlhMjAzNDA0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a8q1JwPJx4LFZPBKlBvvntqtVqLCsBnR_Lp8sv3gkLI";
+
+
+  export {firebaseConfig, TMDB_Access_Key};
